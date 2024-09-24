344155b2d55c0c7061f1cec07eb6acb4404ca7ee
Priyanshu <priyanshupanda36@gmail.com>
2024-09-24 18:10:40 +0530

    Fixed Issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,17 +31,17 @@ const Footer = () => {
                 
                 <div className="flex justify-center space-x-6 text-white mb-6">
                 <a href="https://www.linkedin.com/in/priyanshu-panda/" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin className='text-white transition ease-in-out delay-100 hover:-translate-y-1'></FaLinkedin>
+                <FaLinkedin className='text-white transition ease-in-out delay-100 hover:-translate-y-1' size={24}></FaLinkedin>
               </a>
               <a href="https://leetcode.com/u/its_priyanshu/" target="_blank" rel="noopener noreferrer"> 
-                <LiaExternalLinkAltSolid className='text-white transition ease-in-out delay-100 hover:-translate-y-1 '></LiaExternalLinkAltSolid>
+                <LiaExternalLinkAltSolid className='text-white transition ease-in-out delay-100 hover:-translate-y-1 ' size={24}></LiaExternalLinkAltSolid>
                 </a>
                 <a href="https://www.instagram.com/priyanshuupanda__" target="_blank" rel="noopener noreferrer"> 
-                <FaInstagram className='text-white transition ease-in-out delay-100 hover:-translate-y-1'></FaInstagram></a>
+                <FaInstagram className='text-white transition ease-in-out delay-100 hover:-translate-y-1' size={24}></FaInstagram></a>
                 <a href="https://github.com/thegeek36" target="_blank" rel="noopener noreferrer"> 
-                <FaGithub className='text-white transition ease-in-out delay-100 hover:-translate-y-1'></FaGithub> </a>
+                <FaGithub className='text-white transition ease-in-out delay-100 hover:-translate-y-1' size={24}></FaGithub> </a>
                 <a href="https://x.com/that_tallguy_1" target="_blank" rel="noopener noreferrer"> 
-                <BsTwitterX className='text-white transition ease-in-out delay-100 hover:-translate-y-2'></BsTwitterX> </a>
+                <BsTwitterX className='text-white transition ease-in-out delay-100 hover:-translate-y-2' size={24}></BsTwitterX> </a>
                 </div>
 
                 <button 
